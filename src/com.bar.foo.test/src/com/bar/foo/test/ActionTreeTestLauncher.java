9268f7939e544a7b3bd1c2faec0193c82963e39e
jdeyton <jordan.deyton@gmail.com>
2014-11-11 21:24:56 -0500

    Moved the ActionTreeTestLauncher to the test bundle. Added some content to the launcher. It now opens up a shell with a ToolBar, empty Composite, and some Actions/ActionTrees in the ToolBar.

---
+++ src/com.bar.foo.test/src/com/bar/foo/test/ActionTreeTestLauncher.java
@@ -0,0 +1,82 @@
+package com.bar.foo.test;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.ToolBar;
+
+import com.bar.foo.actiontree.ActionTree;
+
+public class ActionTreeTestLauncher {
+
+	public static void main(String[] args) {
+
+		// Create the Display.
+		Display display = new Display();
+
+		// Create the Shell (window).
+		final Shell shell = new Shell(display);
+		shell.setText("Exodus Mesh Tester");
+		shell.setSize(1024, 768);
+		shell.setLayout(new GridLayout(1, false));
+
+		// Create a ToolBar.
+		ToolBarManager toolBarManager = new ToolBarManager();
+		ToolBar toolBar = toolBarManager.createControl(shell);
+		toolBar.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+		// Create a content Composite to compress the ToolBar.
+		Composite content = new Composite(shell, SWT.NONE);
+		content.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		content.setBackground(display.getSystemColor(SWT.COLOR_DARK_GRAY));
+
+		// Add a basic JFace Action for comparison.
+		Action jfaceAction = new Action("JFace Action") {
+			@Override
+			public void run() {
+				System.out.println("JFace Action");
+			}
+		};
+		toolBarManager.add(jfaceAction);
+
+		// Add a basic ActionTree (default configuration).
+		ActionTree defaultActionTree = new ActionTree();
+		defaultActionTree.fill(toolBarManager);
+
+		// Add a basic ActionTree (no children).
+		ActionTree basicActionTree = new ActionTree();
+		basicActionTree.setAction(new Action("ActionTree") {
+			@Override
+			public void run() {
+				System.out.println("Basic ActionTree");
+			}
+		});
+		basicActionTree.fill(toolBarManager);
+
+		// Refresh the ToolBar.
+		toolBarManager.update(true);
+
+		// Open the shell.
+		shell.open();
+
+		// SOP UI loop.
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		display.dispose();
+
+		// Right now, this is the simplest way to halt the JME3 application.
+		// Otherwise, the program does not actually terminate.
+		System.exit(0);
+
+		return;
+	}
+
+}
