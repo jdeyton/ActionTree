ff7e8c6ece95c988b2dd6660597e482434c83be3
Jordan Deyton <deytonjh@ornl.gov>
2014-11-20 12:36:58 -0500

    Adding initial ActionTreeTester skeleton.

---
+++ src/com.bar.foo.test/src/com/bar/foo/actiontree/test/ActionTreeTester.java
@@ -0,0 +1,39 @@
+package com.bar.foo.actiontree.test;
+
+import org.junit.Test;
+
+import com.bar.foo.actiontree.ActionTree;
+
+/**
+ * This class tests the {@link ActionTree} class's exposed methods.
+ * 
+ * @author Jordan Deyton
+ *
+ */
+public class ActionTreeTester {
+
+	/**
+	 * Checks the copy constructors for both local copies and full-tree copies.
+	 * 
+	 * @see ActionTree#ActionTree(ActionTree)
+	 * @see ActionTree#ActionTree(ActionTree, boolean)
+	 */
+	@Test
+	public void checkCopy() {
+		// TODO
+	}
+
+	/**
+	 * Checks consistency and correctness for equality and hash code methods.
+	 * 
+	 * @see ActionTree#equals(Object)
+	 * @see ActionTree#equals(com.bar.foo.tree.ITree, boolean)
+	 * @see ActionTree#hashCode()
+	 * @see ActionTree#hashCode(boolean)
+	 */
+	@Test
+	public void checkEquality() {
+		// TODO
+	}
+
+}
